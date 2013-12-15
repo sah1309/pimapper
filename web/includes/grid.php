6deb660d54c5ade441b4db9112144b0e263e5db7
Simon Beattie <simon.beattie@randomstorm.com>
2013-12-15 20:48:45 +0000

    Unpacked web directory and added apache2 config file

---
+++ web/includes/grid.php
@@ -0,0 +1,43 @@
+<?php
+//database setting
+$dbhost='localhost';
+$dbuser='root';
+$dbpassword='pa55word';
+$database='pimapper';
+$page = $_GET['page'];
+// get the requested page
+$limit = $_GET['rows'];
+// get how many rows we want to have into the grid
+$sidx = $_GET['sidx'];
+// get index row - i.e. user click to sort
+$sord = $_GET['sord']; // get the direction if(!$sidx)
+$sidx =1;
+// connect to the database
+$db = mysql_connect($dbhost, $dbuser, $dbpassword) or die("Connection Error: " . mysql_error());
+mysql_select_db($database) or die("Error conecting to db.");
+$result = mysql_query("SELECT COUNT(*) AS count FROM host_current");
+$row = mysql_fetch_array($result,MYSQL_ASSOC);
+$count = $row['count'];
+if( $count > 0 ) {
+     $total_pages = ceil($count/$limit);
+}
+else {
+     $total_pages = 0;
+}
+if ($page > $total_pages) $page=$total_pages;
+$start = $limit*$page - $limit; // do not put $limit*($page - 1)
+$SQL = "SELECT * FROM host_current ORDER BY $sidx $sord LIMIT $start , $limit";
+$result = mysql_query( $SQL ) or die("Couldn t execute query.".mysql_error());
+
+$response = new stdClass();
+$response->page = $page;
+$response->total = $total_pages;
+$response->records = $count;
+$i=0;
+while($row = mysql_fetch_array($result,MYSQL_ASSOC)) {
+ $response->rows[$i]['id']=$row['id'];
+ $response->rows[$i]['cell']=array($row['id'],$row['hostname'],$row['hostIP'],date('H:i:s Y-m-d', $row['scanTime']));
+ $i++;
+}
+echo json_encode($response);
+?>
---
+++ web/includes/subgrid.php
@@ -0,0 +1,23 @@
+<?php
+
+$examp = $_GET["q"]; //query number
+
+$id = $_GET['id'];
+
+// connect to the database
+$db = mysql_connect('127.0.0.1', 'root', 'pa55word')
+or die("Connection Error: " . mysql_error());
+
+mysql_select_db('pimapper') or die("Error conecting to db.");
+$SQL = "SELECT portID, scanTime FROM services WHERE hostID=".$id." and scantime = (select max(scantime) from services where hostID=".$id.")";
+$SQL = "SELECT services.portID, ports.service, ports.description, services.scanTime FROM services RIGHT JOIN ports ON ports.port=services.portID AND protocol='tcp' WHERE services.hostID=".$id." AND services.scantime = (select max(scantime) from services where hostID=".$id.") GROUP BY portID;";
+
+$result = mysql_query( $SQL ) or die("Couldnt execute query.".mysql_error());
+
+$response = new stdClass();
+$i=0;
+while($row = mysql_fetch_array($result,MYSQL_ASSOC)) {
+ $response->rows[$i]['cell']=array($row['portID'],$row['service'],$row['description'],date('H:i:s Y-m-d', $row['scanTime']));
+ $i++;
+}
+echo json_encode($response);
