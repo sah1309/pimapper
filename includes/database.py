44061821ed6d5d7e50917826b4419db698e88971
simonbt <simon.beattie@randomstorm.com>
2013-12-15 01:20:07 +0000

    Testing services add to database

--- includes/database.py
+++ includes/database.py
@@ -44,23 +44,12 @@ class host_history(peewee.Model):
     class Meta:
         database = db
 
-class live_ip(peewee.Model):
-    ipaddress = peewee.CharField()
-    scanTime = peewee.CharField()
-
-    class Meta:
-        database = db
-
 try:
         host_current.create_table()
 except:
         pass
 try:
         host_history.create_table()
-except:
-        pass
-try:
-        live_ip.create_table()
 except:
         pass
 try:
