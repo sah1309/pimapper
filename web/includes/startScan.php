0dd2380e9ac127a0cb37bff136e80acf6010ecdb
simonbt <simon.beattie@randomstorm.com>
2013-12-17 15:17:12 +0000

    Added button to test POST to start scan

---
+++ web/includes/startScan.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * web -- startScan.php
+ * User: Simon Beattie
+ * Date: 17/12/2013
+ * Time: 15:01
+ */
+
+var_dump($_POST);
+
+
+die();
+
+$type = $_POST['type'];
+$options = $_POST['opt'];
+
+$command = '/usr/bin/sucks/dick.py ';
+
+$line = $command . ' ' . $options;
+
+exec($line, $returnedOutput, $returnCode);
+
+var_dump($returnedOutput);
+
+var_dump($returnCode);
+
+
+
+$descriptors = array(
+    0 => array("pipe", "r"),  // stdin is a pipe that the child will read from
+    1 => array("pipe", "w"),  // stdout is a pipe that the child will write to
+    2 => array("pipe", "w") // stderr is a file to write to
+);
+
+$cwd = '/tmp';
+
+$process = proc_open($command, $descriptors, $pipes, $cwd);
+
+ob_end_flush();
+
+if (is_resource($process)) {
+    // $pipes now looks like this:
+    // 0 => writeable handle connected to child stdin
+    // 1 => readable handle connected to child stdout
+    // Any error output will be appended to /tmp/error-output.txt
+
+    while(!feof($pipes[1]))
+    {
+        echo fread($pipes[1], 1024);
+    }
+}
--- web/includes/stormmapper.php
+++ web/includes/stormmapper.php
@@ -2,6 +2,8 @@
 header('Content-Type:application/json');
 #Checks to see if stormmapper is running
 
+
+
 if (file_exists("/var/run/stormmapper.pid"))
 {
 	$is_running['isRunning'] = true;
