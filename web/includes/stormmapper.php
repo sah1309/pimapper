35694f8d9c767364800f7dfabc5696159782ca4b
simonbt <simon.beattie@randomstorm.com>
2013-12-17 16:00:26 +0000

    Added button to test POST to start scan

--- web/includes/stormmapper.php
+++ web/includes/stormmapper.php
@@ -2,13 +2,11 @@
 header('Content-Type:application/json');
 #Checks to see if stormmapper is running
 
-
-
 if (file_exists("/var/run/stormmapper.pid"))
 {
 	$is_running['isRunning'] = true;
 }
  else {
-    $is_running['isRunning'] = true;
+    $is_running['isRunning'] = false;
 }
 echo json_encode($is_running);
