1f5325ff663f7f48a1a10758a0dc952f526e8eab
simonbt <simon.beattie@randomstorm.com>
2013-12-17 08:44:09 +0000

    Added pid file, logging to file and exit statuses

--- stormmapper.py
+++ stormmapper.py
@@ -10,64 +10,96 @@ parser.add_argument("--output", help="output JSON after scan", action="store_tru
 args = parser.parse_args()
 
 import commands
+import logging
 import json
+import sys, os
 from includes.settings import bcolors
 from netaddr import IPNetwork
 from includes.database import *
 from includes.network import network
 from includes.scanners import *
 
-def create_json_report():
-    print 'Creating report..'
-    results = dict()
-    for i in host_current.select(host_current.id, host_current.hostIP, host_current.hostname).where(host_current.scanTime == timestamp):
-        os = os_match.get(os_match.hostID == i.id, os_match.scanTime == timestamp)
-        results[i.hostname] = {'ip' : i.hostIP, 'os' : {'type' : os.os, 'confidence': os.confidence}}
-        results[i.hostname].update({'scan':{}})
-        for portresults in services.select(services.hostID, services.portID).where(services.scanTime == timestamp).where(services.hostID == i.id):
-            results[i.hostname]['scan'].update({ portresults.portID : { 'description' : ports.get(ports.port == portresults.portID).description }})
-    return json.dumps(results, indent=2)
+#Setup Logging
+LOG_FILENAME = '/var/log/stormmapper.log'
+logging.basicConfig(filename=LOG_FILENAME,level=logging.DEBUG,)
 
-primaryIf='eth0'
-IPChecks = network()
-currentIP = commands.getoutput("/sbin/ifconfig").split("\n")[1].split()[1][5:]
-currentRange = str(IPNetwork(currentIP + "/" + str(IPChecks.getBits(IPChecks.get_netmask(primaryIf)))).network) + "/" + str(IPChecks.getBits(IPChecks.get_netmask(primaryIf)))
-timestamp = int(time.time())
+#Create pidfile
+pid = str(os.getpid())
+pidfile = "/var/run/stormmapper.pid"
 
-print bcolors.OKGREEN
-print "   _____ _                       __  __              "
-print "  / ____| |                     |  \/  |             "
-print " | (___ | |_ ___  _ __ _ __ ___ | \  / | __ _ _ __   "
-print "  \___ \| __/ _ \| '__| '_ ` _ \| |\/| |/ _` | '_ \  "
-print "  ____) | || (_) | |  | | | | | | |  | | (_| | |_) | "
-print " |_____/ \__\___/|_|  |_| |_| |_|_|  |_|\__,_| .__/  "
-print bcolors.OKBLUE + "  Network Mapping and Discovery       " + bcolors.OKGREEN + "       | |     "
-print bcolors.OKBLUE + "  Simon Beattie // @Si_Bt // 2013     " + bcolors.OKGREEN + "       |_|     "
-print bcolors.ENDC
+if os.path.isfile(pidfile):
+    print "%s already exists, exiting" % pidfile
+    sys.exit()
+else:
+    file(pidfile, 'w').write(pid)
 
-if not (args.target or args.port or args.os):
-    print "Type stormmapper.py --help for options"
-    print
-    quit()
+def main():
+    try:
+        def create_json_report():
+            print 'Creating report..'
+            results = dict()
+            for i in host_current.select(host_current.id, host_current.hostIP, host_current.hostname).where(host_current.scanTime == timestamp):
+                os = os_match.get(os_match.hostID == i.id, os_match.scanTime == timestamp)
+                results[i.hostname] = {'ip' : i.hostIP, 'os' : {'type' : os.os, 'confidence': os.confidence}}
+                results[i.hostname].update({'scan':{}})
+                for portresults in services.select(services.hostID, services.portID).where(services.scanTime == timestamp).where(services.hostID == i.id):
+                    results[i.hostname]['scan'].update({ portresults.portID : { 'description' : ports.get(ports.port == portresults.portID).description }})
+            return json.dumps(results, indent=2)
 
-current_scan = scanners()
+        primaryIf='eth0'
+        IPChecks = network()
+        currentIP = commands.getoutput("/sbin/ifconfig").split("\n")[1].split()[1][5:]
+        currentRange = str(IPNetwork(currentIP + "/" + str(IPChecks.getBits(IPChecks.get_netmask(primaryIf)))).network) + "/" + str(IPChecks.getBits(IPChecks.get_netmask(primaryIf)))
+        timestamp = int(time.time())
 
-if args.target:
-    current_scan.discovery_scan(args.target,timestamp)
-    print bcolors.OKGREEN + "Scan Completed!" + bcolors.ENDC
+        print bcolors.OKGREEN
+        print "   _____ _                       __  __              "
+        print "  / ____| |                     |  \/  |             "
+        print " | (___ | |_ ___  _ __ _ __ ___ | \  / | __ _ _ __   "
+        print "  \___ \| __/ _ \| '__| '_ ` _ \| |\/| |/ _` | '_ \  "
+        print "  ____) | || (_) | |  | | | | | | |  | | (_| | |_) | "
+        print " |_____/ \__\___/|_|  |_| |_| |_|_|  |_|\__,_| .__/  "
+        print bcolors.OKBLUE + "  Network Mapping and Discovery       " + bcolors.OKGREEN + "       | |     "
+        print bcolors.OKBLUE + "  Simon Beattie // @Si_Bt // 2013     " + bcolors.OKGREEN + "       |_|     "
+        print bcolors.ENDC
 
-maxTime = []
-for i in host_current.select(host_current.scanTime):
-    maxTime.append(i.scanTime)
-lastScan = max(maxTime)
+        if not (args.target or args.port or args.os):
+            print "Type stormmapper.py --help for options"
+            print
+            quit()
 
-if args.port:
-    current_scan.port_scan(lastScan)
-    print bcolors.OKGREEN + "Scan Completed!" + bcolors.ENDC
+        current_scan = scanners()
 
-if args.os:
-    current_scan.os_fingerprint(lastScan)
-    print bcolors.OKGREEN + "Scan Completed!" + bcolors.ENDC
+        if args.target:
+            current_scan.discovery_scan(args.target,timestamp)
+            print bcolors.OKGREEN + "Scan Completed!" + bcolors.ENDC
 
-if args.output:
-    print create_json_report()
+        maxTime = []
+        for i in host_current.select(host_current.scanTime):
+            maxTime.append(i.scanTime)
+        lastScan = max(maxTime)
+
+        if args.port:
+            current_scan.port_scan(lastScan)
+            print bcolors.OKGREEN + "Scan Completed!" + bcolors.ENDC
+
+        if args.os:
+            current_scan.os_fingerprint(lastScan)
+            print bcolors.OKGREEN + "Scan Completed!" + bcolors.ENDC
+
+        if args.output:
+            print create_json_report()
+
+        #Cleanup pid file
+        os.unlink(pidfile)
+
+    except KeyboardInterrupt:
+        print "Shutdown requested.. exiting"
+        os.unlink(pidfile)
+    except Exception:
+        logging.exception('StormMapper Error')
+        print "An error has occurred - Check the logs!"
+        os.unlink(pidfile)
+
+if __name__ == "__main__":
+    main()
