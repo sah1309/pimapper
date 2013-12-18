628be27d91e49e4ea3208caca7b3fbf585d24749
simonbt <simon.beattie@randomstorm.com>
2013-12-18 08:16:02 +0000

    Updated js script locations

---
+++ web/js/stormmapper.js
@@ -0,0 +1,60 @@
+/**
+ * User: simonbeattie
+ * Date: 18/12/2013
+ * Time: 08:14
+ */
+
+function ValidateIPaddress(inputText)
+{
+    var ipformat = /^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/;
+    if(inputText.value.match(ipformat))
+    {
+        return true;
+    }
+
+    return true;
+
+}
+
+function processFormData()
+{
+
+    var name_element = document.getElementById('txt_name');
+
+    var win = ValidateIPaddress(name_element.value);
+    alert(win);
+
+    if(ValidateIPaddress(name_element.value))
+    {
+        $.post("/includes/startScan.php",
+            {
+                opt:"-o" + name_element.value
+            });
+        isRunning();
+    }
+    else
+    {
+        alert("Invalid IP");
+    }
+
+}
+
+$(document).ready(function(){
+    $("#startOS").click(function(){
+        $.post("/includes/startScan.php",
+            {
+                opt:"-o"
+            });
+        isRunning();
+    });
+});
+$(document).ready(function(){
+    $("#startPort").click(function(){
+        $.post("/includes/startScan.php",
+            {
+                opt:"-p"
+            });
+        isRunning();
+    });
+});
+
