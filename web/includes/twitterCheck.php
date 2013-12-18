48e30de8f5c2712af63e2f68b37572fc3368de52
simonbt <simon.beattie@randomstorm.com>
2013-12-18 20:28:37 +0000

    Adding save twitter auth data to MySQL

--- web/includes/twitterCheck.php
+++ web/includes/twitterCheck.php
@@ -7,16 +7,31 @@ require_once(__DIR__ . '/bootstrap.php');
 $checkLogin = $pdo->query("SELECT oauth_token, oauth_token_secret FROM twitter WHERE user = " . $user . ";");
 $twitterCreds = $checkLogin->fetchAll(PDO::FETCH_ASSOC);
 
-print_r($checkLogin);
+if (!$twitterCreds){
 
-if (empty($_SESSION['access_token']) || empty($_SESSION['access_token']['oauth_token']) || empty($_SESSION['access_token']['oauth_token_secret'])) {
-    $isLoggedin['isLoggedin'] = false;
+    if (empty($_SESSION['access_token']) || empty($_SESSION['access_token']['oauth_token']) || empty($_SESSION['access_token']['oauth_token_secret'])) {
+
+        $isLoggedin['isLoggedin'] = false;
+    }
+    else
+    {
+        $isLoggedin['isLoggedin'] = true;
+    }
+
 }
 else
 {
+    //Get details from DB
     $isLoggedin['isLoggedin'] = true;
 }
 
 echo json_encode($isLoggedin);
+
+
+
+print_r($twitterCreds);
+
+
+
 
 
