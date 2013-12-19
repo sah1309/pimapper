3d023f4a2d04412114d5bba2e9d8b24a7f03ca99
simonbt <simon.beattie@randomstorm.com>
2013-12-19 08:46:53 +0000

    Hopefully fixed post requests

--- web/includes/TwitterClass.php
+++ web/includes/TwitterClass.php
@@ -36,6 +36,8 @@ class TwitterFuncs extends StatusAbstract{
 
     function sendDM($user, $message)
     {
+        echo $user;
+        echo $message;
         $dm = $this->getOauth()->post('direct_messages/new', array('user_id' => $user, 'text' => $message));
         return $dm;
     }
